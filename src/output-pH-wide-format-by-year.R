17e8454056979989c20773b74ef10eb20c09f3d2
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-05-21 17:53:56 -0700

    Ouput horizon and pH in wide format for Dave to review

---
+++ src/output-pH-wide-format-by-year.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+df <- read_csv("output/soilCore1998To2015ShallowDeepMergedByHorizon_20180926.csv")
+
+pH.wide.horizon <- df %>% 
+  select(Year, ID2, Latitude, Longitude, TopDepth, 
+         BottomDepth, Horizon) %>%
+  spread(Year, Horizon) %>% 
+  rename(Horizon1998 = `1998`,
+         Horizon2008 = `2008`,
+         Horizon2015 = `2015`)
+
+pH.wide.pH <- df %>% 
+  select(Year, ID2, Latitude, Longitude, TopDepth, 
+         BottomDepth, pH) %>%
+  spread(Year, pH) %>% 
+  rename(pH1998 = `1998`,
+         pH2008 = `2008`,
+         pH2015 = `2015`)
+
+wide <- full_join(pH.wide.horizon, pH.wide.pH, by = c("ID2", 
+                                                      "Latitude", 
+                                                      "Longitude", 
+                                                      "TopDepth", 
+                                                      "BottomDepth")) %>% 
+  select(ID2, Latitude, Longitude, TopDepth, BottomDepth,
+         Horizon1998, pH1998,
+         Horizon2008, pH2008,
+         Horizon2015, pH2015)
+
+dateToday <- format(Sys.Date(), "%Y%m%d")
+
+write_csv(wide, paste("output/Horizon-pH-wide_", dateToday, ".csv", sep = ""), na = "")
