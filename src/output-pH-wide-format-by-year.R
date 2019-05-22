d42c43c8240870c883faa09218d67412def0d879
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-05-21 17:55:07 -0700

    Arranged by id2 maybe?

--- src/output-pH-wide-format-by-year.R
+++ src/output-pH-wide-format-by-year.R
@@ -26,8 +26,9 @@ wide <- full_join(pH.wide.horizon, pH.wide.pH, by = c("ID2",
   select(ID2, Latitude, Longitude, TopDepth, BottomDepth,
          Horizon1998, pH1998,
          Horizon2008, pH2008,
-         Horizon2015, pH2015)
+         Horizon2015, pH2015) %>% 
+  arrange(by = ID2)
 
 dateToday <- format(Sys.Date(), "%Y%m%d")
 
-write_csv(wide, paste("output/Horizon-pH-wide_", dateToday, ".csv", sep = ""), na = "")
+write_csv(wide, paste("output/horizon-pH-wide_", dateToday, ".csv", sep = ""), na = "")
